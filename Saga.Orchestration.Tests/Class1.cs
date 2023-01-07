accb54cec888dbf40bc2d56e0bf6ad36ee782358
wieslaw <wieslawo@poczta.onet.pl>
2023-01-07 22:23:43 +0100

    Added test project

---
+++ Saga.Orchestration.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Saga.Orchestration.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
